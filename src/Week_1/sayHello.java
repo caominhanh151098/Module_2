e973535421623c5b43af05f5ab14442b6265979b
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-10 14:41:12 +0700

    fix git

---
+++ src/Week_1/sayHello.java
@@ -0,0 +1,15 @@
+package Week_1;
+
+import java.util.Scanner;
+
+public class sayHello {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
