091d525dbbcb45239ff76fb37c346199010b11de
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-10 16:47:30 +0700

    add

---
+++ src/Week_2/prime_less_than_100.java
@@ -0,0 +1,33 @@
+package Week_2;
+
+import java.util.Scanner;
+
+public class prime_less_than_100 {
+    public static void main(String[] args) {
+        int N = 2;
+        while (N < 100) {
+            if (isPrime(N)) {
+                System.out.println(N);
+                N++;
+            }
+            else {
+                N++;
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        } else {
+            int i = 2;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+}
