133700ddb2566f7f1091ce17bed079d3dbf4836a
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-10 15:15:31 +0700

    add

---
+++ src/Week_2/greate_common_factor.java
@@ -0,0 +1,30 @@
+package Week_2;
+
+import java.util.Scanner;
+
+public class greate_common_factor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap so a:");
+        int a = scanner.nextInt();
+        System.out.print("Nhap so b:");
+        int b = scanner.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("Khong co UCLN");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            }
+            else b -= a;
+        }
+
+        System.out.printf("UCLN la: %d", a);
+    }
+}
