504932f6b5402601697110df349c01c75b01de0b
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-11 09:36:32 +0700

    add

---
+++ src/Week_2/find_element_in_array.java
@@ -0,0 +1,21 @@
+package Week_2;
+
+import java.util.Scanner;
+
+public class find_element_in_array {
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        int find = -1;
+        for (int i = 0; i < students.length; i ++) {
+
+            if (students[i].equals(name)) {
+                find = i;
+            }
+        }
+        System.out.printf(find != -1 ? ("Position of the students in the list" + name + "is: " + find) : "Not found!");
+    }
+}
