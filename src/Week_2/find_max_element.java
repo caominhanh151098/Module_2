8287e0b02b97a4a49d4c26738bb0cde23139e421
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-11 09:49:36 +0700

    fix

--- src/Week_2/find_max_element.java
+++ src/Week_2/find_max_element.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 
 public class find_max_element {
     public static void main(String[] args) {
+        int size;
         Scanner scanner = new Scanner(System.in);
-        int[] array = new int[20];
+        System.out.print("Enter a size:");
+        do {
+            size = Integer.parseInt(scanner.nextLine());
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100);
+//            array[i] = (int) (Math.random() * 100);
+            System.out.printf("Enter element %d: ", i + 1);
+            array[i] = Integer.parseInt(scanner.nextLine());
         }
 
         for (int i = 0; i < array.length; i++) {
