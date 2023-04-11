3cc370a24f502f84a55f7990722b6c130908c412
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-11 10:01:06 +0700

    fix

---
+++ src/Week_2/convert_temperature.java
@@ -0,0 +1,38 @@
+package Week_2;
+
+import java.util.Scanner;
+
+public class convert_temperature {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            double f, c;
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Fahrenheit: ");
+                    f = scanner.nextDouble();
+                    c = (5.0 / 9) * (f - 32);
+                    System.out.printf("Celsiuc: %.3f\n", c);
+                    break;
+                case 2:
+                    System.out.print("Enter Celsius: ");
+                    c = scanner.nextDouble();
+                    f = (9.0 / 5) * c + 32;
+                    System.out.printf("Fahrenheit: %.3f\n", f);
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong choice!");
+            }
+
+        } while (true);
+    }
+}
