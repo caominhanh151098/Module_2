796b719155ed9c8c8092da78c969b9a9b59eb54e
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-11 13:51:42 +0700

    add

--- src/BT/Bai3.java
+++ src/BT/Bai3.java
@@ -2,15 +2,15 @@ package BT;
 
 public class Bai3 {
     public static void main(String[] args) {
-        int[] array = new int[50];
-        for (int i = 0; i < array.length; i ++) {
-            array[i] = (int) (Math.random() * 100);
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 20 - 10);
             System.out.print(array[i] + "  ");
         }
         System.out.println();
         System.out.println("///////////////////////");
 
-        for (int i = 0; i < array.length - 1; i++) {
+        /*for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
                     int temp = array[i];
@@ -21,8 +21,21 @@ public class Bai3 {
         }
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "  ");
+        }*/
+        int sum = 0;
+        long multi = 1;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                sum += array[i];
+                multi *= array[i];
+                count++;
+            }
         }
+        double avg = sum / count;
+        System.out.printf("SUM: %d\n", sum);
+        System.out.printf("MULTI: %d\n", multi);
+        System.out.printf("AVG: %f\n", avg);
+
     }
-
-
 }
