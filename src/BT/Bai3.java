95968686db1dc88a44b1416d567bfef8a49b635d
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-12 16:39:23 +0700

    fix

--- src/BT/Bai3.java
+++ src/BT/Bai3.java
@@ -4,18 +4,18 @@ public class Bai3 {
     public static void main(String[] args) {
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 20 - 10);
+            array[i] = (int) (Math.random() * 40 - 20);
             System.out.print(array[i] + "  ");
         }
         System.out.println();
         System.out.println("///////////////////////");
 
         for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
                 }
             }
         }
