0c4230d8f91dd8b40a2a89952937129ed1d9047f
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-14 17:09:35 +0700

    add

---
+++ src/Week_1/Class/Fan/Main.java
@@ -0,0 +1,22 @@
+package Week_1.Class.Fan;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Fan fan_1 = new Fan();
+        fan_1.setSpeed(fan_1.FAST);
+        fan_1.setRadius(10);
+        fan_1.setColor("yellow");
+        fan_1.setOn(true);
+
+        Fan fan_2 = new Fan();
+        fan_2.setSpeed(fan_2.MEDIUM);
+        fan_2.setRadius(5);
+
+        System.out.println(fan_1.toString());
+        System.out.println(fan_2.toString());
+    }
+}
