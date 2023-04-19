73441a06c60fabf047249b78072d0f1def2604b9
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-19 15:28:03 +0700

    add

---
+++ src/Week_2/inheritance/triangle/Main.java
@@ -0,0 +1,40 @@
+package Week_2.inheritance.triangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a Triangle: ");
+        System.out.print("Enter side 1: ");
+        double side1 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter side 2: ");
+        double side2 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter side 3: ");
+        double side3 = Double.parseDouble(scanner.nextLine());
+
+        Triangle mTriangle = new Triangle(side1, side2, side3);
+        System.out.println(mTriangle.toString());
+
+        System.out.println("Do u want to change color and fill? (y/n)");
+        char choice = scanner.nextLine().charAt(0);
+
+        if (choice == 'y') {
+            System.out.print("Enter color: ");
+            String color = scanner.nextLine();
+            System.out.println("And fill? (y/n)");
+            choice = scanner.nextLine().charAt(0);
+            boolean filled = false;
+            if (choice == 'y') {
+                filled = true;
+            }
+            mTriangle.setColor(color);
+            mTriangle.setFilled(filled);
+        }
+
+        System.out.println(mTriangle.toString());
+        System.out.println(mTriangle.display());
+
+    }
+}
