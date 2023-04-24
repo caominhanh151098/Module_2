c1e927fe3c2c6a9519b66ca546ef69c973d0246e
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-24 15:05:08 +0700

    add

---
+++ src/Week_2/clean_code/refactoring_2/FizzBuzz.java
@@ -0,0 +1,19 @@
+package Week_2.clean_code.refactoring_2;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+
+        if(isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if(isFizz)
+            return "Fizz";
+
+        if(isFizz)
+            return "Buzz";
+
+        return number + "";
+    }
+}
