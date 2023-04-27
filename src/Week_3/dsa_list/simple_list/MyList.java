241401e8ab8ad4cd7d02cd52deb9b1d3128b555f
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-27 10:23:38 +0700

    add

---
+++ src/Week_3/dsa_list/simple_list/MyList.java
@@ -0,0 +1,32 @@
+package Week_3.dsa_list.simple_list;
+
+import java.util.Arrays;
+
+public class MyList<E> {
+    public int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+        System.out.println(size);
+    }
+
+    public void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public E get(int i) {
+        if (i >= size || i < 0)
+            throw new ArrayIndexOutOfBoundsException("Index: " + i + ", Size: " + size);
+        return (E) elements[i];
+    }
+}
