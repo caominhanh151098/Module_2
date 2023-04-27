edb7f01ab09897dbbc7ed2d8546c057887d5b5b4
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-27 09:11:13 +0700

    add

--- src/Week_4/regex/crawl_news/CrawlNews/CrawlNewsExample.java
+++ src/Week_4/regex/crawl_news/CrawlNews/CrawlNewsExample.java
@@ -1,5 +1,6 @@
-package Week_4.regex.songlist;
+package Week_4.regex.crawl_news.CrawlNews;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -7,22 +8,25 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CrawlSongExample {
+public class CrawlNewsExample {
     public static void main(String[] args) {
+        ;
+        URL url;
         try {
-            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            url = new URL("https://dantri.com.vn/the-gioi.htm");
             Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
             scanner.useDelimiter("\\Z");
             String content = scanner.next();
             scanner.close();
             content = content.replaceAll("\\n+", "");
-            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
-            Matcher m = p.matcher(content);
-            while (m.find()) {
-                System.out.println(m);
+            Pattern pattern = Pattern.compile("article-title\">(.*?)\">(.*?)</a></h3>");
+            Matcher matcher = pattern.matcher(content);
+            while (matcher.find()) {
+                System.out.println(matcher.group(2));
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Lỗi");
         }
+
     }
 }
