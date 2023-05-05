91723c424dfb1a8390bc24115ea0c624f419be8c
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-05-05 08:50:10 +0700

    add

---
+++ src/Week_4/dsa_stack/StackToBinary/MainStack.java
@@ -0,0 +1,23 @@
+package Week_4.dsa_stack.StackToBinary;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class MainStack {
+    public static void main(String[] args) {
+        System.out.print("Nhập số thập phân: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+        Stack<Integer> stack = new Stack();
+
+        while (input != 0) {
+            stack.push(input % 2);
+            input /= 2;
+        }
+
+        System.out.println("Kết quả nhị phân: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + "");
+        }
+    }
+}
